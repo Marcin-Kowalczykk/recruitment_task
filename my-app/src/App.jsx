cb6c83580ec897815bb1fed38539ae4addf1d811
Marcin  Kowalczyk <kowalczyk.marcin1997@gmail.com>
2022-01-22 13:18:18 +0100

    global styles and main  theme added

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 
+import GlobalStyle from './styles/GlobalStyle'
+import { lightTheme } from './styles/theme'
+import { ThemeProvider } from 'styled-components'
+
 function App () {
   return (
-    <div>
-      <h1>Application cleaned</h1>
-      <h2>Ready to work</h2>
-    </div>
+    <ThemeProvider theme={lightTheme}>
+      <GlobalStyle />
+      <h1>Volkswagen Golf IV 1.6 TDI</h1>
+    </ThemeProvider>
   )
 }
 
---
+++ my-app/src/styles/theme.js
@@ -0,0 +1,18 @@
+export const lightTheme = {
+  color: {
+    mainBg: '#ebebebe8',
+    headerBg: '#8a03b0e0',
+    listLightBg: 'white',
+    listDarkBg: '#b478d96b',
+    errorColor: '#d50707f5',
+    lightFont: 'white',
+    mediumFont: '#000000a6',
+    darkFont: 'black'
+  },
+  fontSize: {
+    extraSmall: '12px',
+    small: '14px',
+    large: '18px',
+    extraLarge: '22px'
+  }
+}
