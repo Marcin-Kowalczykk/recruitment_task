65a1ea2cf3a12ef4b057616939b35c50d2671f02
Marcin  Kowalczyk <kowalczyk.marcin1997@gmail.com>
2022-01-23 21:41:00 +0100

    data.json moved to public for fetch and change title of file

---
+++ my-app/src/components/ListElements/TableHeader/TableHeader.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import { Th } from './styled'
+
+const TableHeader = () => {
+  return (
+    <tr>
+      <Th>Nazwa</Th>
+      <Th>Model</Th>
+      <Th>Silnik</Th>
+      <Th>Dostępność</Th>
+      <Th>Akcje</Th>
+    </tr>
+
+  )
+}
+
+export default TableHeader
