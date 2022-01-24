dae4172baf13ab620aab125b93187ebecd642330
Marcin  Kowalczyk <kowalczyk.marcin1997@gmail.com>
2022-01-24 01:30:16 +0100

    show table annimation added

--- my-app/src/components/ListElements/CarTable/styled.js
+++ my-app/src/components/ListElements/CarTable/styled.js
@@ -1,4 +1,9 @@
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
+
+const animationShow = keyframes`
+    0% {opacity: 0;}
+    100% {opacity: 1;}
+`
 
 export const TableContainer = styled.table`
   background: ${(props) => props.theme.color.lightListBg};
@@ -9,4 +14,5 @@ export const TableContainer = styled.table`
   border-spacing: 0;
   box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
   text-align: left;
+  animation: ${animationShow} 1s ease-in-out;
 `
